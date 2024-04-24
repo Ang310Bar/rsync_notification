a4557e54c21783e2bddd16cbe17775877112495b
Ang310Bar <167202987+Ang310Bar@users.noreply.github.com>
2024-04-24 10:55:20 +0200

    Update rsyncWindows.ps1

--- rsyncWindows.ps1
+++ rsyncWindows.ps1
@@ -46,7 +46,7 @@ function InviaEmail {
 
     # Verifica se sia l'oggetto che il corpo dell'email sono vuoti
     if ([string]::IsNullOrEmpty($oggetto_email) -or [string]::IsNullOrEmpty($corpo_email)) {
-        Write-Host "Oggetto o corpo dell'email vuoti. L'email non verrà inviata."
+        Write-Host "Oggetto o corpo dell'email vuoti. L'email non verrà inviata." -ForegroundColor Red
         return
     }
 
@@ -57,12 +57,12 @@ function InviaEmail {
         } else {
             Send-MailMessage -From $EmailFrom -To $email_destinatario -Subject $oggetto_email -Body $corpo_email -SmtpServer "smtp.office365.com" -Port 587 -UseSsl -Credential $Credential -ErrorAction Stop
         }
-        Write-Host "Email inviata con successo."
+        Write-Host "Email inviata con successo." -ForegroundColor Green
         "Email inviata con successo." | Out-File -FilePath "$script_output" -Append
     } catch {
-        Write-Host "Si e' verificato un errore durante l'invio dell'email: $($_.Exception.Message)"
+        Write-Host "Si e' verificato un errore durante l'invio dell'email: $($_.Exception.Message)" -ForegroundColor Red
         "Si e' verificato un errore durante l'invio dell'email: $($_.Exception.Message)" | Out-File -FilePath "$script_output" -Append
-        Write-Host "Riprovero' l'invio dell'email tra un minuto..."
+        Write-Host "Riprovero' l'invio dell'email tra un minuto..." -ForegroundColor Yellow
         "Riprovero' l'invio dell'email tra un minuto..." | Out-File -FilePath "$script_output" -Append
         Start-Sleep -Seconds 60 
         InviaEmail -email_destinatario $email_destinatario -comune $comune -oggetto_email $oggetto_email -corpo_email $corpo_email
@@ -90,7 +90,7 @@ $Credential = New-Object System.Management.Automation.PSCredential($EmailFrom, $
 
 $comando_completo = "$comando > >(tee $file_output) 2> >(tee $file_errore >&2)"
 
-Write-Host "Sto per iniziare l'rsync..."
+Write-Host "Sto per iniziare l'rsync..." -ForegroundColor Green
 "Sto per iniziare l'rsync..." | Out-File -FilePath "$script_output" -Append
 Write-Host "3"
 Start-Sleep -Seconds 1
@@ -103,7 +103,7 @@ Start-Sleep -Seconds 1
 $retryCount = 1
 do {
     Start-Process -FilePath $cygwinExe -ArgumentList "-l", "-i", "-c", "`"$comando_completo`"" -Wait -NoNewWindow
-
+   
     if (Select-String -Path "$cygwinPath$file_errore" -Pattern "Warning: Permanently added") {
         $numero_righe=(Get-Content $cygwinPath$file_errore).Count
         if ($numero_righe -eq 1) {
@@ -115,28 +115,51 @@ do {
     $stato_uscita = Get-Content -Path "$cygwinPath$file_errore"
     # Controlla lo stato di uscita del comando
     if ($null -eq $stato_uscita) {
-        Write-Host "Rsync della cartella completato con successo."
+        Write-Host "Rsync della cartella completato con successo." -ForegroundColor Green
         "Rsync della cartella completato con successo." | Out-File -FilePath "$script_output" -Append
         $oggetto_email = "RSYNC TERMINATO CON SUCCESSO"
         $corpo_email = "Ti comunico che l'rsync del repository, avviato per il comune di $comune, e' stato completato con successo! Se desideri visualizzare il log, lo trovi al percorso '$cygwinPath\tmp\log.txt'."
         # Invia un'email di notifica di successo
-        InviaEmail -email_destinatario $email_destinatario -comune $comune -oggetto_email $oggetto_email -corpo_email $corpo_email
+        #InviaEmail -email_destinatario $email_destinatario -comune $comune -oggetto_email $oggetto_email -corpo_email $corpo_email
         break
     } else {
-        Write-Host "Rsync della cartella interrotto con errore."
-        "Rsync della cartella interrotto con errore." | Out-File -FilePath "$script_output" -Append
-        $oggetto_email = "RSYNC INTERROTTO - ERRORE"
-        $corpo_email = "Ti comunico che l'rsync del repository, avviato per il comune di $comune, si e' interrotto non portando a termine il trasferimento. Se desideri visualizzare i log, li trovi nel percorso '$cygwinPath\tmp\', questo e' lo stato di uscita del comando: `n$stato_uscita"
-        $retryCount++
+        Write-Host "Rsync della cartella interrotto con errore. Stato uscita: $stato_uscita . Tentativo n $retryCount" -ForegroundColor Red
+        "Rsync della cartella interrotto con errore. Stato uscita: $stato_uscita . Tentativo n $retryCount" | Out-File -FilePath "$script_output" -Append
+         $retryCount++
         if ($retryCount -ge 5) {
-            Write-Host "Numero massimo di tentativi raggiunto. Interrompo il processo."
+            Write-Host "Numero massimo di tentativi raggiunto. Interrompo il processo." -ForegroundColor Red
             "Numero massimo di tentativi raggiunto. Interrompo il processo." | Out-File -FilePath "$script_output" -Append
-            InviaEmail -email_destinatario $email_destinatario -comune $comune -oggetto_email $oggetto_email -corpo_email $corpo_email
+            #InviaEmail -email_destinatario $email_destinatario -comune $comune -oggetto_email $oggetto_email -corpo_email $corpo_email
             break
         } else {
-            Write-Host "Riprovo il processo. Tentativo n $($retryCount)"
+            $ip = "8.8.8.8"
+            if (!(Test-Connection -ComputerName $ip -Count 1 -Quiet)) {
+                Write-Host "Connessione tra le due macchine interrotta! Attendo 60 secondi e riprovo..." -ForegroundColor Red
+                "Connessione tra le due macchine interrotta! Attendo 60 secondi e riprovo..." | Out-File -FilePath "$script_output" -Append
+                $riconn = 1
+                #Attende fino a 15 minuti poi ferma lo script
+                while ($riconn -le 30) {
+                    Start-Sleep -Seconds 30
+                    Write-Host "Tentativo di riconnessione: $riconn" -ForegroundColor Yellow
+                    "Tentativo di riconnessione: $riconn" | Out-File -FilePath "$script_output" -Append
+                    if (Test-Connection -ComputerName $ip -Count 1 -Quiet) {
+                        Write-Host "Connessione ristabilita." -ForegroundColor Green
+                        "Connessione ristabilita." | Out-File -FilePath "$script_output" -Append
+                        break
+                    }
+                    $riconn++
+                }
+                if ($riconn -gt 30) {
+                    Write-Host "Tentativi di riconnessione esauriti. Fermo lo script." -ForegroundColor Red
+                    "Tentativi di riconnessione esauriti. Fermo lo script." | Out-File -FilePath "$script_output" -Append
+                    $oggetto_email = "RSYNC INTERROTTO PER DISCONNESSIONE"
+                    $corpo_email = "Ti comunico che l'rsync del repository, avviato per il comune di $comune , si è interrotto a causa di una disconnessione. Ho fatto 30 tentativi di riconnessione ma senza successo. Troverai il riepilogo completo ($outputFile) e l'output dello script ($logFile) nella cartella dove hai eseguito il comando. Questo invece è il riepilogo degli errori: `n$errore"
+                    #InviaEmail -email_destinatario $email_destinatario -comune $comune -oggetto_email $oggetto_email -corpo_email $corpo_email
+                    break
+                }
+            }
+            Write-Host "Riprovo il processo. Tentativo n $($retryCount)" -ForegroundColor Yellow
             "Riprovo il processo. Tentativo n $($retryCount)" | Out-File -FilePath "$script_output" -Append
         }
     }
 } while ($true)
-
